568f9a9fc0e1557788d8ce045cb2b97f67c5113e
Santiago Sobral <santiago.sobral.perez@gmail.com>
2024-11-27 17:55:28 -0300

    aiudaaaa

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -20,7 +20,7 @@ int main()
 
         while (dep != 0)
         {
-            grafoMisiones.agregarAristaDirigida(idMision, dep, 1);
+            grafoMisiones.agregarAristaDirigida(dep, idMision, 1);
             cin >> dep;
         }
         listaIdCiudadMisiones[idMision] = idCiudad;
@@ -43,6 +43,50 @@ int main()
         cin >> ciudadSalida >> ciudadEntrada >> tiempo;
         grafoCiudades.agregarAristaNoDirigida(ciudadSalida, ciudadEntrada, tiempo);
     }
+    cout << "Ciudad inicial: ";
+    cout << ciudadSalida;
+    int* posiblesMisiones = grafoMisiones.getMisionesPosibles();
+    while (posiblesMisiones[0] != 0){
+        int min = 2147483647;
+        int** minimaDistancia = grafoCiudades.dijkstra(C);
+        int idSiguienteMision = 0;
+        for (int i = 0; i < M && posiblesMisiones[i] != 0; i++){
+            if(minimaDistancia[0][i] < min){
+                min = minimaDistancia[0][i];
+                idSiguienteMision = posiblesMisiones[i];
+            }
+        }
+        int* caminoAlaMision = caminoInvertido(minimaDistancia[1], C);
+        for (int i = 1; i < caminoAlaMision[0]; i++){
+            cout << listaNombreCiudades[caminoAlaMision[i]]; //recorrido desde C hasta donde se hace la mision
+            cout << " -> ";
+        }
+        cout << "Mision: ";
+        cout << listaNombreMisiones[idSiguienteMision];
+        cout << " - ";
+        cout << listaIdCiudadMisiones[idSiguienteMision];
+        cout << " - Tiempo de viaje: ";
+        cout << min << endl;
+        grafoMisiones.misionCompletada(idSiguienteMision);
+        posiblesMisiones = grafoMisiones.getMisionesPosibles();
+    }
 
     return 0;
+}
+int* caminoInvertido(int* aRecorrer, int desde){
+    int largo = 1;
+    int paso = desde;
+    while (paso != 0){
+        largo ++;
+        paso = aRecorrer[paso];
+    }
+    int* ret = new int[largo];
+    paso = desde;
+    while (paso != 0){
+        ret[largo] = paso; 
+        paso = aRecorrer[paso];
+        largo --;
+    }
+    ret[0] = largo - 1;
+    return ret;
 }
--- tads/nuevoGrafo.cpp
+++ tads/nuevoGrafo.cpp
@@ -35,4 +35,68 @@ public:
     {
         matriz[i][j] = peso;
     }
+    void misionCompletada (int i){
+        for(int j = 0 ; j <= cantVertices ; j++){
+            matriz[j][i] = 0;
+            matriz[i][j] = -1;
+        } 
+    }
+    int* getMisionesPosibles (){
+        bool conseguiMision = false;
+        int* ret = new int[cantVertices + 1]();
+        int mison = 0;
+        for (int i = 0; i <= cantVertices && !conseguiMision; i++){
+            bool dependencia = false ; 
+            for (int j = 0; j <= cantVertices && !dependencia ; j++){
+                if(matriz[i][j] == 1 || matriz[i][j] == -1){
+                    dependencia = true;
+                }
+                if( j = cantVertices && !dependencia){
+                    ret[mison] = i;
+                    mison++;
+                }
+            }
+        }
+        return ret;
+    }
+    int minDistancia(int* ret, bool* visitado) {
+        int min = 2147483647;
+        int minIndex = -1;
+        for (int i = 0; i <= cantVertices; i++) {
+            if (!visitado[i] && ret[i] <= min) {
+                min = ret[i];
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+    int** dijkstra(int desde){
+        int** ret = new int*[2];
+        //Distancia desde el origen al resto del grafo
+        ret[0] = new int[cantVertices];
+        //Anterior desde cualquier valor hasta el origen
+        ret[1] = new int[cantVertices];
+        bool* visitado = new bool[cantVertices+1];
+
+   
+        for (int i = 0; i <= cantVertices; i++) {
+            ret[0][i] = 2147483647;
+            visitado[i] = false;
+        }
+        ret[0][desde-1] = 0;
+        ret[1][desde-1] = 0;
+        for (int i = 0; i <= cantVertices ; i++) {
+        
+            int u = minDistancia(ret[0], visitado);
+            visitado[u] = true;
+
+            for (int v = 0; v < cantVertices; v++) {
+                if (!visitado[v] && matriz[u][v] != 0 &&  ret[0][u] != 2147483647 && ret[0][u] + matriz[u][v] < ret[0][v]) {
+                    ret[0][v] = ret[0][u] + matriz[u][v];
+                    ret[1][v] = u;
+                }
+            }
+        }
+        return ret;
+    }
 };
