f58bc0928c9c4d1f1e479ed84eb5aeb8babde4b0
Santiago Sobral <santiago.sobral.perez@gmail.com>
2024-11-27 23:00:51 -0300

    ahora si anda

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -10,10 +10,13 @@ int* caminoInvertido(int* aRecorrer, int desde){
         largo ++;
         paso = aRecorrer[paso];
     }
+    //cout << desde<<endl;
     int* ret = new int[largo];
     paso = desde;
     ret[0] = largo-1;
     while (paso != 0){
+        //cout << paso;
+        // cout << "  " <<endl;
         ret[largo-1] = paso; 
         paso = aRecorrer[paso];
         largo --;
@@ -63,21 +66,21 @@ int main()
     
     cout << "Ciudad inicial: ";
     
-    cout << listaNombreCiudades[ciudadSalida]<<endl;
+    cout << listaNombreCiudades[ciudadInicio]<<endl;
     int distanciaTotal = 0;
     int* posiblesMisiones = grafoMisiones.getMisionesPosibles();
     while (posiblesMisiones[0] != 0){
         int min = 2147483647;
-        int** minimaDistancia = grafoCiudades.dijkstra(ciudadSalida);
+        int** minimaDistancia = grafoCiudades.dijkstra(ciudadInicio);
         int idSiguienteMision = 0;
         for (int i = 0; i < M && posiblesMisiones[i] != 0; i++){
-            if(minimaDistancia[0][i] < min){
-                min = minimaDistancia[0][posiblesMisiones[i]];
+            if(minimaDistancia[0][listaIdCiudadMisiones[posiblesMisiones[i]]] < min){
+                min = minimaDistancia[0][listaIdCiudadMisiones[posiblesMisiones[i]]];
                 idSiguienteMision = posiblesMisiones[i];
-                ciudadSalida = posiblesMisiones[i];
+                ciudadInicio = listaIdCiudadMisiones[posiblesMisiones[i]];
             }
         }
-        int* caminoAlaMision = caminoInvertido(minimaDistancia[1], ciudadSalida);
+        int* caminoAlaMision = caminoInvertido(minimaDistancia[1], ciudadInicio);
         for (int i = 1; i <= caminoAlaMision[0]; i++){
             cout << listaNombreCiudades[caminoAlaMision[i]]; //recorrido desde C hasta donde se hace la mision
             cout << " -> ";
